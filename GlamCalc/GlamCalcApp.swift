65bf9eab64bc76fc1659b48c8d317ebbfc2813e7
Chao Chen <cherrycc93@gmail.com>
2025-11-17 11:10:36 -0500

    Initial Commit

---
+++ GlamCalc/GlamCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlamCalcApp.swift
+//  GlamCalc
+//
+//  Created by Chao Chen on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GlamCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
